775bfce28802416a7c9793702096c7f4a4a9751f
Felipe Serra <felipeserra@hotmail.com>
2024-05-21 15:21:56 -0300

    add

--- src/WhatsappIA.API/Program.cs
+++ src/WhatsappIA.API/Program.cs
@@ -1,8 +1,6 @@
-
-var port = Environment.GetEnvironmentVariable("PORT") ?? "8081";
 var builder = WebApplication.CreateBuilder(args);
-builder.WebHost.UseUrls($"http://*:{port}");
-
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8081";
+//builder.WebHost.UseUrls($"http://localhost:{port}");
 // Add services to the container.
 
 builder.Services.AddControllers();
@@ -13,13 +11,10 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-//if (app.Environment.IsDevelopment())
-//{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-//}
+app.UseSwagger();
+app.UseSwaggerUI();
 
-//app.UseHttpsRedirection();
+app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
